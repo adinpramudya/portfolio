609fe153ffada8f77590298bf7a8df20d8867731
Adinda Pramudya Ramadhan <adinramadhan10@gmail.com>
2022-10-04 06:20:29 +0700

    delete router and change main js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Particles from "vue3-particles";
-import router from "./router.js";
 const app = createApp(App);
 app.use(Particles);
-app.use(router);
 app.mount("#app");
 
 export default router;
